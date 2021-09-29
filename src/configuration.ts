39d99ff5172decefe42b3ecc71e13db2b30b2b3b
Bing Ren <bingtimren@gmail.com>
2021-09-30 09:57:02 +1000

    change configuration schema

---
+++ src/configuration.ts
@@ -0,0 +1,22 @@
+/**
+ * A selector is a GlobPattern (https://code.visualstudio.com/api/references/vscode-api#GlobPattern)
+ * If workspaceFolder is specified, it must be identical to a "path" in the "folders" settings in the workspace file, and
+ * the GlobPattern is a relative pattern (https://code.visualstudio.com/api/references/vscode-api#RelativePattern).
+ * If workspaceFolder is not set, basePath should not be set.
+ * 
+ */
+
+export interface SelectorConfiguration {
+    workspaceFolder?: string,
+    basePath?: string,
+    includeGlob: string,
+    excludeGlob?: string
+}
+
+/**
+ * A Pocket is a group of selectors for filtering files, for example, "configurations", "view controllers", "unit tests"
+ */
+export interface PocketConfiguration {
+    name: string,
+    selectors: SelectorConfiguration[]
+}
