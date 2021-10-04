04ad8a23de6bf368ba030c85abc4a39376eb9343
Bing Ren <bingtimren@gmail.com>
2021-10-04 19:36:20 +1100

    open command realised

--- src/tidy-view.ts
+++ src/tidy-view.ts
@@ -1,4 +1,4 @@
-import { Event, EventEmitter, ProviderResult, TreeDataProvider, TreeItem, TreeItemCollapsibleState, Uri, workspace, WorkspaceFolder } from "vscode";
+import { Event, EventEmitter, ProviderResult, TreeDataProvider, TreeItem, TreeItemCollapsibleState, Uri, workspace, WorkspaceFolder, commands } from "vscode";
 import { Selector } from "./selector";
 import { UriNode } from "./tidy-view-node";
 
@@ -8,7 +8,7 @@ const PATH_SEPARATOR = "/";
 export class TidyExplorerTreeDataProvider implements TreeDataProvider<UriNode>{
     private onDidChangeTreeDataEmitter: EventEmitter<UriNode | undefined> = new EventEmitter<UriNode | undefined>();
     readonly onDidChangeTreeData: Event<void | UriNode | null | undefined> = this.onDidChangeTreeDataEmitter.event;
-    private readonly root : UriNode = UriNode.createRoot();
+    private readonly root: UriNode = UriNode.createRoot();
     private selectors: Selector[] = [];
     public async addSelector(selector: Selector) {
         // add all files
@@ -16,10 +16,10 @@ export class TidyExplorerTreeDataProvider implements TreeDataProvider<UriNode>{
             this.addFile(uri, selector);
         };
         // listen to file changes
-        selector.onDidFileCreate((created)=>{
+        selector.onDidFileCreate((created) => {
             this.addFile(created, selector);
         });
-        selector.onDidFileDelete((deleted)=>{
+        selector.onDidFileDelete((deleted) => {
             this.deleteFile(deleted, selector.workspaceFolder);
         });
         this.onDidChangeTreeDataEmitter.fire(undefined);
@@ -30,32 +30,38 @@ export class TidyExplorerTreeDataProvider implements TreeDataProvider<UriNode>{
     }
     private addFile(uri: Uri, fromSelector: Selector) {
         const relativePath = this.getRelativePath(uri, fromSelector.workspaceFolder);
-        const affectedNode = this.root.addChildren(uri, fromSelector, relativePath, 0); 
+        const affectedNode = this.root.addChildren(uri, fromSelector, relativePath, 0);
         this.onDidChangeTreeDataEmitter.fire(affectedNode);
     }
-    private deleteFile(uri: Uri, knownWorkspaceFolder: WorkspaceFolder|undefined) {
+    private deleteFile(uri: Uri, knownWorkspaceFolder: WorkspaceFolder | undefined) {
         const relativePath = this.getRelativePath(uri, knownWorkspaceFolder);
         const affectedNode = this.root.delChildren(relativePath, 0);
         this.onDidChangeTreeDataEmitter.fire(affectedNode);
     }
-    private getRelativePath(uri:Uri, knownWorkspaceFolder: WorkspaceFolder|undefined):string {
+    private getRelativePath(uri: Uri, knownWorkspaceFolder: WorkspaceFolder | undefined): string {
         const workspaceFolder = knownWorkspaceFolder || workspace.getWorkspaceFolder(uri) as WorkspaceFolder;
         return workspace.asRelativePath(uri, true);
     }
     getTreeItem(element: UriNode): TreeItem | Thenable<TreeItem> {
-        const item = new TreeItem(element.name, element.children?TreeItemCollapsibleState.Collapsed:TreeItemCollapsibleState.None);
-        item.resourceUri = element.uri!;
+        const item = new TreeItem(element.uri!, element.children ? TreeItemCollapsibleState.Collapsed : TreeItemCollapsibleState.None);
+        if (!element.children) {
+            item.command = {
+                command: "vscode.open",
+                title: "Open",
+                arguments: [item.resourceUri],
+            }
+        };
         return item;
     }
     getChildren(element?: UriNode): ProviderResult<UriNode[]> {
-            return Object.values(element? 
-                (element.children?Object.values(element.children):[]) :
-                (this.root.children?Object.values(this.root.children):[])
-            );
+        return Object.values(element ?
+            (element.children ? Object.values(element.children) : []) :
+            (this.root.children ? Object.values(this.root.children) : [])
+        );
     }
 }
 
-export function returnNextSeparatorAndName(relativePath: string, startPosition: number):[number, string] {
+export function returnNextSeparatorAndName(relativePath: string, startPosition: number): [number, string] {
     // find next section 
     const nextSeparator = relativePath.indexOf(PATH_SEPARATOR, startPosition);
     // the child node's name, either a file or dir name
